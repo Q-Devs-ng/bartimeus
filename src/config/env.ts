450eb223124974dd089fd8500408cf9aa121b7c0
Lord Bee <akinwandeakinboluwarin@gmail.com>
2022-07-24 19:06:53 +0100

    env-validation

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,4 @@
+import { AppEnvironment } from '@src/internals/env';
 import { IsEnum, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class Env {
@@ -14,6 +15,15 @@ export class Env {
 
   @IsNotEmpty()
   SERVICE_SECRET: string;
+
+  @IsNotEmpty()
+  REDIS_URL: string;
+
+  @IsNotEmpty()
+  MONGODB_URL: string;
+
+  @IsNotEmpty()
+  JWT_SECRET: string;
 
   @IsNotEmpty()
   SESSION_TTL = 30000;
