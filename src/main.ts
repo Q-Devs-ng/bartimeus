5c9c30a7335b497967f9135469a5796d372510a9
Lord Bee <akinwandeakinboluwarin@gmail.com>
2022-07-24 18:13:55 +0100

    swagger setup chore

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Talking Stage app API')
+    .setDescription('Talking Stage app API')
+    .setVersion('1.0')
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, doc);
+
   await app.listen(3000);
 }
 bootstrap();
