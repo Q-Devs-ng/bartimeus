d6b85c6ab42177d82eef9065deb6e075d84ca748
Akinwande Akinboluwarin <akinwandeakinboluwarin@gmail.com>
2022-07-24 18:36:08 +0100

    swagger setup chore (#2)
    * swagger setup chore
    
    * swagger setup
    
    * swagger setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiForbiddenResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('hello')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOkResponse({ description: 'The server is running' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
   getHello(): string {
     return this.appService.getHello();
   }
