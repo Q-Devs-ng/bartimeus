06b55e5294c3e113e62120bfde926a61e19ef297
Akinwande Akinboluwarin <akinwandeakinboluwarin@gmail.com>
2022-07-26 10:28:14 +0100

    env-validation (#3)
    * env-validation
    
    * updated env-validation
    
    * env validation

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,8 @@
-import { IsEnum, IsNotEmpty, IsNumber } from 'class-validator';
+import { AppEnvironment } from '@src/internals/env';
+import { plainToInstance } from 'class-transformer';
+import { IsEnum, IsNotEmpty, IsNumber, validateSync } from 'class-validator';
 
-export class Env {
+export class EnvironmentVariables {
   @IsNotEmpty()
   @IsEnum(() => AppEnvironment)
   NODE_ENV: AppEnvironment;
@@ -16,5 +18,28 @@ export class Env {
   SERVICE_SECRET: string;
 
   @IsNotEmpty()
+  REDIS_URL: string;
+
+  @IsNotEmpty()
+  MONGODB_URL: string;
+
+  @IsNotEmpty()
+  JWT_SECRET: string;
+
+  @IsNotEmpty()
   SESSION_TTL = 30000;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
 }
